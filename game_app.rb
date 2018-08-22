ec3a42a309484dfc0658545f62b45aac62e6a05a
ks1venberg <venberg.dev@gmail.com>
2018-08-22 16:28:38 +0300

    modified playzone, added flow with control buttons
    initial structure _ step2

--- game_app.rb
+++ game_app.rb
@@ -2,9 +2,9 @@ require 'shoes'
 
  Shoes.app(title: "welcome to minigames pack!", width: 400, height: 500) do
 
-   background gray
+   background skyblue
 
-   stack(margin: 10) {
+   stack(margin: 10) do
       flow do
         button "Slot machine"
         para "Try to win in this classic casino game!"
@@ -13,12 +13,18 @@ require 'shoes'
         button "Tic-Tac-Toe"
         para "Miss about school years?! If yes, try it!"
       end
-    }
+    end
 
-    stack do
+    #stack do
+      fill lightsalmon
       # widht & height values used from window_App size
       rect(70, 120, self.width - 140, self.height - 240, corners=4)
-      background lightcoral
+    #end
+
+    flow left: self.width/2-self.width/10, top: self.height-110  do
+      button "Restart"
+      button "End"
+      button "Exit App"
     end
 
  end
