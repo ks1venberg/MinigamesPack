531b3aa00dac7f54817d7c7cfb3349d39aeb36cb
ks1venberg <venberg.dev@gmail.com>
2018-08-28 22:56:22 +0300

    new App structrure started, refactoring with methods

--- game_app.rb
+++ game_app.rb
@@ -1,40 +1,41 @@
 require 'shoes'
 
-class Gameconstuct
-  def initialize
-    @balance = balance
+Shoes.app(title: "welcome to minigames pack!", width: 400, height: 440) do
+  
+  # Main layout ___________________________________________________________________________________
+  background darkgreen
+
+  def create_playzone
+    # widht & height values used from window_App size
+    playzone = rect(70, 110, self.width*0.65, self.height*0.55, corners=4, fill: darkgoldenrod) 
   end
 
-  attr_accessor :balance
+  # Methods and variables _________________________________________________________________________
+  def show_balance
+    @balance = 200
+    @bal_msg = para "Your balance: ", strong(@balance.to_s), align: 'center', top: 80
+  end
 
-end
 
- Shoes.app(title: "welcome to minigames pack!", width: 400, height: 460) do
-
-   background darkgreen
-
-   stack(margin: 10) do
+    stack(margin: 10) do
       flow do
-        button "Slot machine", width: 80 do
-          #balance_field
-          @balance = 200
-          @bal_msg.replace "Your balance: ", strong(@balance.to_s), align: 'center', top: 70
-          # @bal_txt = strong "Your balance: ", align: 'center', top: 60
+        btn_slotmachine = button "Slot machine", width: 80 do
+          show_balance
+        
+
         end
         para "Try this classic casino game!", margin: 4
       end
 
       flow do
-        button "Tic-Tac-Toe", width: 80
+        btn_tictac = button "Tic-Tac-Toe", width: 80 do
+
+        end
         para "Miss about school years?! Let's play!", margin: 4
       end
-
-      @bal_msg = para ""
     end
 
-    fill darkgoldenrod
-    # widht & height values used from window_App size
-    rect(70, 110, self.width - 140, self.height - 200, corners=4)
+    create_playzone
 
     flow left: self.width/2-self.width/5, top: self.height-80  do
       button "Restart", width: 60, margin: 2
@@ -42,4 +43,4 @@ end
       button "Exit App", width: 60, margin: 2
     end
 
- end
+end
