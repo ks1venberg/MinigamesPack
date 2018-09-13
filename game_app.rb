2301809cf02e157a9d6ab2ebf355fd714480cfd8
ks1venberg <venberg.dev@gmail.com>
2018-09-13 20:50:46 +0300

    new app construction - all changable objects packed into stacks
    in this way it`s faster & easier to remove/change/duplicate it

--- game_app.rb
+++ game_app.rb
@@ -7,6 +7,11 @@ Shoes.app(title: "welcome to minigames pack!", width: 400, height: 440) do
   # Methods and variables _________________________________________________________________________
   @boxes = {}
   @syms = {}
+  @playzone = rect
+  @stackplay = stack
+  @stackboxes = stack
+  @stacksyms = stack
+
 
   def sizes
     # size of general layout
@@ -21,7 +26,7 @@ Shoes.app(title: "welcome to minigames pack!", width: 400, height: 440) do
     #borders of playzone
       @xplayleft = @playzone.left
       @yplaytop = @playzone.top
-    #box params (52 px)
+    #box params: 52 px each side
       @boxside = @plwidth / 5
       @step = 10
     # start position for adding boxes to playzone
@@ -29,6 +34,12 @@ Shoes.app(title: "welcome to minigames pack!", width: 400, height: 440) do
       @xboxleft = @xplayleft + @step*3
     # above param used only for TicTacToe
       @yboxtop = @yplaycenter - @boxside - @step
+  end
+
+  def create_playzone
+    @stackplay = stack(left: (self.width - self.width*0.65)/2 - 10, top: 95) do
+      @playzone = rect(0, 0, self.width*0.65, self.height*0.55, corners=4, fill: darkgoldenrod)
+    end
   end
 
   def show_balance
@@ -48,7 +59,7 @@ Shoes.app(title: "welcome to minigames pack!", width: 400, height: 440) do
   end
 
   def click_handle
-    s = stack left: 120, top: @yplaycenter do
+    @stacksyms = stack left: 120, top: @yplaycenter do
       @boxes.each_value do |value|
         value.each do |x, y|
           i = 1.upto(3)
@@ -93,51 +104,58 @@ Shoes.app(title: "welcome to minigames pack!", width: 400, height: 440) do
     sizes
        # first, is to check wich game has started and so decide about 1st box position
        # @balance variable assigned only in Slot machine, so if its true- there will be created 3 boxes in the middle
-      @yplaycenter = @yboxtop if !@balance
-        1.upto 9 do |z|
-          box = rect(@xboxleft, @yplaycenter, @boxside, @boxside, corners=4, fill: white)
-           # here is creating array with boxes names & coordinates
-          @boxes["box"<<z.to_s] = ["#{box.left}, #{box.top}"]
-           # moving boxes over the playzone
-          @xboxleft = ("#{box.left}".to_i + @boxside + @step)
+      @stackboxes = stack(left: @xplayleft, top: @yplaytop) do
+        @yplaycenter = @yboxtop if !@balance
+          1.upto 9 do |z|
+            box = rect(@xboxleft, @yplaycenter, @boxside, @boxside, corners=4, fill: white)
+             # here is creating array with boxes names & coordinates
+            @boxes["box"<<z.to_s] = ["#{box.left}, #{box.top}"]
+             # moving boxes over the playzone
+            @xboxleft = ("#{box.left}".to_i + @boxside + @step)
 
-          # end loop after 3rd box created (rule for Slot machine)
-          break if @balance && z == 3
-          # change x,y when row is ended
-          @xboxleft = (@xplayleft + @step*3) if (z == 3 || z == 6)
-          @yplaycenter += (@boxside + @step) if (z == 3 || z == 6)
-        end
+            # end loop after 3rd box created (rule for Slot machine)
+            break if @balance && z == 3
+            # change x,y when row is ended
+            @xboxleft = (@xplayleft + @step*3) if (z == 3 || z == 6)
+            @yplaycenter += (@boxside + @step) if (z == 3 || z == 6)
+          end
+      end
   end
  
     stack(margin: 10) do
+
       flow(margin: 2) do
         btn_slotmachine = button "Slot machine", width: 90, heigth: 35, margin_right: 4 do
           show_balance
           create_handle
           create_boxes
           g1start_alert
-        
-
         end
         para strong "Play this classic casino game!", stroke: white
       end
 
       flow(margin: 2) do
         btn_tictac = button "Tic-Tac-Toe", width: 90, heigth: 35, margin_right: 4 do
-          create_boxes
+          #@stackplay.remove
+          @stackplay.clear{create_playzone} if @boxes.size == 3
+          #@balance = nil
+          #create_boxes
         end
         para strong "Miss about school years? Try it!", stroke: white
       end
-    end
 
-    # widht & height values used in % of window_App size
-    @playzone = rect(70, 110, self.width*0.65, self.height*0.55, corners=4, fill: darkgoldenrod)
+      # widht & height values used in % of window_App size
+      # @stackplay = stack do
+        create_playzone
+        #@playzone = rect(70, 30, self.width*0.65, self.height*0.55, corners=4, fill: darkgoldenrod)
+      # end
 
-    #control buttons
-    flow(left: self.width*0.27, top: self.height-80)  do
-      button "Restart", width: 60, margin: 2
-      button "End", width: 60, margin: 2
-      button "Exit App", width: 60, margin: 2
+      #control buttons
+      flow(left: self.width*0.27, top: self.height-80)  do
+        button "Restart", width: 60, margin: 2
+        button "Exit App", width: 60, margin: 2
+      end
+
     end
 
 end
